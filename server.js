6298acd8b3daa30dec7dbe9e46aed739d7185e96
Eran Avichzer <eranavichzer@gmail.com>
2022-05-15 13:53:41 +0300

    Fix cookies

--- server.js
+++ server.js
@@ -13,7 +13,15 @@ const userService = require('./services/user.service')
 const app = express()
 
 // Config the Express App
-app.use(cors());
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')))
+  } else {
+    const corsOptions = {
+      origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+      credentials: true
+    }
+    app.use(cors(corsOptions))
+  }
 app.use(express.static('public')) // Load the public folder (th e fornt end)
 app.use(cookieParser()) // Manage Cookies
 app.use(express.json()) // What is doing? It parses incoming JSON requests and puts the parsed data in req.body.
@@ -143,7 +151,7 @@ app.post('/api/login', (req, res) => {
                 // const cookies = new universalCookie(req.headers.cookie);
                 // cookies.set('loginToken', loginToken, { path: '/' });
                 // console.log(cookies.get('loginToken'));
-                //res.cookie('loginToken', loginToken)
+                res.cookie('loginToken', loginToken)
                 // console.log('loginToken', req.cookies.loginToken);
                 res.send(user)
             } else {
